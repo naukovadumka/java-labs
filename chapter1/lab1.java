919813588be287b3139d4a90384219847d507738
naukovadumka <naukovadumka85@gmail.com>
2017-03-01 23:02:17 +0100

    three lines

--- chapter1/lab1.java
+++ chapter1/lab1.java
@@ -1,5 +1,7 @@
 class Main {
 	public static void main(String[] args) {
 		System.out.println("Adam is sleeping");
-	}
+		System.out.println("Kira is sleeping");
+		System.out.println("Night pam param");
+}
 }
